80b19330725e656234c0d41153b1e3c6c665a18f
phuocnguyen <phuoc.nguyen2704@hcmut.edu.vn>
2024-05-20 22:46:31 +0700

    v1

--- js/script.js
+++ js/script.js
@@ -62,3 +62,57 @@ window.onscroll = () =>{
       body.classList.remove('active');
    }
 }
+
+
+// ------------------------------------------------------- OOP ------------------------------------------------------
+// ----------------------------------------------------------- OOP class user --------------------------------------------------------------------
+class user{
+
+   #name;
+   #email;
+   department;
+   constructor(name, email ,department){
+       this.#name=name
+       this.#email=email
+       this.department=department
+   }
+
+   get_name(){
+       return this.#name;
+   }
+   set_name(name){
+       return this.#name=name;
+   }
+
+   
+   get_email(){
+       return this.#email;
+   }
+   set_email(email){
+       return this.#email=email;
+   }
+
+
+
+}
+
+
+class course {
+   constructor(course_name, course_id, course_teacher, danhsachlop,size){
+       this.course_name=course_name;
+       this.course_id=course_id;
+       this.course_teacher=course_teacher;
+       this.danhsachlop=danhsachlop;
+       this.size=size;
+   }
+   
+   
+
+}
+
+
+
+
+
+
+
